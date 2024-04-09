a2d343e07a3513fa00d760497379ea83d85da981
Riloscode <riloscode@gmail.com>
2024-04-09 18:27:14 -0400

    Initial commit

---
+++ mental_health_visual.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from sklearn.preprocessing import LabelEncoder
+from category_encoders import LeaveOneOutEncoder
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+df = pd.read_csv('data_science/data_lake/Mental_Health_Dataset.csv')
+
+
+# print(f"Number of Duplicate rows :", df.duplicated().sum())
+df = df.drop_duplicates()
+
+# print(f"Number of null rows :", df.isnull().sum())
+
+df.dropna(inplace=True)
+
+# print(f"Number of null rows :", df.isnull().sum())
+
+le =LabelEncoder()
+df["Gender"] = le.fit_transform(df["Gender"])
+df["self_employed"] = le.fit_transform(df["self_employed"])
+df["family_history"] = le.fit_transform(df["family_history"])
+df["treatment"] = le.fit_transform(df["treatment"])
+df["Coping_Struggles"] = le.fit_transform(df["Coping_Struggles"])
+
+data = pd.get_dummies(data=df, columns=["Occupation", "Days_Indoors", "Growing_Stress",
+            "Changes_Habits", "Mental_Health_History", "Work_Interest", "Social_Weakness",
+            "mental_health_interview", "care_options"])
+
+data = pd.get_dummies(data=data, columns=["Mood_Swings"])
+
+data.drop("Timestamp", axis=1, inplace=True)
+
+leave_encoder = LeaveOneOutEncoder()
+data["Country"] = leave_encoder.fit_transform(data["Country"], data.iloc[:, -3])
+
+print(data.head())
+
+y = data.iloc[:, -3:]
+X = data.drop(data.iloc[:, -3:], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, shuffle=True, random_state=42)
+print(y.head())
+print(X.head())
